5a517eb959ea07c5ba1e94272e709755f4253b19
isma <ismaelmartin302@gmail.com>
2024-05-06 00:11:25 +0200

    usuario creado login arreglado + register (aun por editar)

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('username')
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('nickname')
+                    ->maxLength(50)
+                    ->default(null),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->maxLength(9)
+                    ->default(null),
+                Forms\Components\TextInput::make('biography')
+                    ->maxLength(160)
+                    ->default(null),
+                Forms\Components\TextInput::make('location')
+                    ->maxLength(30)
+                    ->default(null),
+                Forms\Components\TextInput::make('website')
+                    ->maxLength(100)
+                    ->default(null),
+                Forms\Components\DatePicker::make('birthday'),
+                Forms\Components\FileUpload::make('avatar')
+                    ->required()
+                    ->image()
+                    ->maxSize(528)
+                    ->avatar()
+                    ->default('default.jpg'),
+                Forms\Components\FileUpload::make('banner')
+                    ->required()
+                    ->image()
+                    ->maxSize(528)
+                    ->default('defaultbanner.jpg'),
+                // Forms\Components\TextInput::make('type')
+                //     ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+                Tables\Columns\ImageColumn::make('avatar')
+                    ->circular()
+                    ->visibility('private')
+                    ->label(''),
+                Tables\Columns\TextColumn::make('nickname')
+                    ->searchable()
+                    ->default(fn ($record) => $record->username ?? null),
+                Tables\Columns\TextColumn::make('username')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('biography')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('location')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('website')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('birthday')
+                    ->date()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\ImageColumn::make('banner')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('type')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                // Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            // 'view' => Pages\ViewUser::route('/{record}'),
+            // 'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () { return redirect('/admin'); })->name('login');
