65ce76cf767e9927d57038e023e447038e178f05
isma <ismaelmartin302@gmail.com>
2024-05-06 16:47:57 +0200

    mas migraciones

---
+++ database/seeders/EventTagSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
