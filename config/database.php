6a3a5e5604ddb043ca270bd4ac8a2d4d8572dd1b
isma <ismaelmartin302@gmail.com>
2024-06-17 03:22:07 +0200

    arreglando mas cosas

--- config/database.php
+++ config/database.php
@@ -81,7 +81,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOSTNAME', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
